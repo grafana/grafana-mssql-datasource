1396dc5051503a8c75cca799e513c159587553d1
Ryan Morris <ryan.morris@grafana.com>
2026-05-05 12:33:59 -0500

    Frontend externalization: build tooling and vendored utilities

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
-// force timezone to UTC to allow tests to work regardless of local timezone
-// generally used by snapshots, but can affect specific tests
-process.env.TZ = 'UTC';
+// We set this specifically for 2 reasons.
+// 1. It makes sense for both CI tests and local tests to behave the same so issues are found earlier
+// 2. Any wrong timezone handling could be hidden if we use UTC/GMT local time (which would happen in CI).
+process.env.TZ = 'Pacific/Easter'; // UTC-06:00 or UTC-05:00 depending on daylight savings
 
 module.exports = {
-  // Jest configuration provided by Grafana scaffolding
   ...require('./.config/jest.config'),
 };
